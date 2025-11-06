d5b3320a210a45f8d578898f3b7ecac6ecd9d876
pietrocontadini-smartpeg <Pietro.Contadini@smartpeg.eu>
2025-11-06 14:34:53 +0100

    up to date

--- core/ConfigManager.js
+++ core/ConfigManager.js
@@ -53,7 +53,7 @@ export class ConfigManager {
       console.log("API key non esiste su env");
       process.exit(1);
     }
-    console.log(`setted api key: ${process.env.OPENAI_API_KEY}`);
+    console.log(`setted api key length: ${process.env.OPENAI_API_KEY.length}`);
    /* const packName = this.options.stepspack;
     this.stepsPackPath = `./stepspacks/${packName}`;
 
